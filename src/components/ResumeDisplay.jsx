f96be5f03bd273b98841927c3f6e7b71fb6074a1
Aurorn <aurornispro@gmail.com>
2024-12-16 16:58:30 -0500

    Updated styling and further construction of tables of information

--- src/components/ResumeDisplay.jsx
+++ src/components/ResumeDisplay.jsx
@@ -6,29 +6,43 @@ function ResumeDisplay({ generalInfo, educationInfo, practicalInfo }) {
     <div className="resume-display">
       <section className="info-section">
         <section className="name-title">
-          <h2>{generalInfo.name}</h2>
-          <p className="job-title-text">Job Title:</p>
+          <h2 className="name-title-text">{generalInfo.name}</h2>
+          <p className="job-title-text">{generalInfo.jobTitle}</p>
         </section>
         <section className="info-text-container">
-          <h3></h3>
           <p className="info-text">{generalInfo.email}</p>
           <p className="info-text">{generalInfo.phone}</p>
         </section>
       </section>
     
-      <section>
-        <h3>Work Experience</h3>
-        <p><strong>Company Name:</strong> {practicalInfo.companyName}</p>
-        <p><strong>Position Title:</strong> {practicalInfo.positionTitle}</p>
-        <p><strong>Responsibilities:</strong> {practicalInfo.mainTasks}</p>
-        <p><strong>From:</strong> {practicalInfo.dateFrom} <strong>Until:</strong> {practicalInfo.dateUntil}</p>
+      <section className="work-section">
+        <h3 className="work-title">Work Experience</h3>
+        <section className="info-container">
+          <section className="time-container">
+            <p>{practicalInfo.dateFrom} <strong> - </strong> {practicalInfo.dateUntil}</p>
+            <p>{practicalInfo.Location}</p>
+          </section>
+          <section className="work-info">
+            <p><strong>{practicalInfo.positionTitle}</strong></p>
+            <p>{practicalInfo.companyName}</p>
+            <p>{practicalInfo.mainTasks}</p>
+          </section>
+        </section>
       </section>
 
-      <section>
-        <h3>Education</h3>
-        <p><strong>School:</strong> {educationInfo.schoolName}</p>
-        <p><strong>Title of Study:</strong> {educationInfo.titleOfStudy}</p>
-        <p><strong>Date of Study:</strong> {educationInfo.dateOfStudy}</p>
+      <section className="edu-section">
+        <h3 className="edu-title">Education</h3>
+        <section className="info-container">
+          <section className="time-container">
+            <p>{educationInfo.dateOfStudy}</p>
+            <p>{practicalInfo.Location}</p>
+          </section>
+          <section className="edu-info">
+            <p><strong>{educationInfo.schoolName}</strong> </p>
+            <p>{educationInfo.titleOfStudy}</p>
+            
+          </section>
+        </section>
       </section>
     </div>
   );
